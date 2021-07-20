2735063113a29d3aeb9af66b41818f8965f0571f
AnthonyDWeb <delforge.anthony@gmail.com>
2021-07-20 12:32:21 +0200

    Problem with calculateWater

--- chapitre01/elsy/src/App.js
+++ chapitre01/elsy/src/App.js
@@ -13,25 +13,77 @@ const stepsMin = 0;
 const stepsmax = 50000;
 
 class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      water: 1.5,
+      heart: 120,
+      temperature: -10,
+      steps: 3000
+    }
+  }
+
+  onHeartChange = (e) => {
+    this.setState({ heart: e.target.value })
+  }
+
+  onStepsChange = (e) => {
+    this.setState({ steps: e.target.value })
+  }
+
+  onTemperatureChange = (e) => {
+    this.setState({ temperature: e.target.value })
+    this.calculateWater(e)
+  }
+
+  calculateWater = (e) => {
+
+
+    console.log('state temperature value: ' + this.state.temperature)
+    console.log('target value: ' + e.target.value)
+
+    if ((e.target.value - 20) >= 0 && this.state.temperature < e.target.value) {
+
+      console.log("condition rempli")
+
+      this.setState(() => {
+        return {
+          water: this.state.water + ((e.target.value - 20) * 0.02)
+        }
+      })
+
+    } else if ((e.target.value - 20) >= 0 && this.state.temperature > e.target.value) {
+      this.setState((prevState) => {
+        return {
+          water: prevState.water - ((e.target.value - 20) * 0.02)
+        }
+      })
+      console.log("condition non rempli")
+    }
+
+  }
+
   render() {
+
     return (
-    
+
       <div className="container-fluid">
         <div className="row">
-          <p>Heart: {heartMin}</p>
-          <p>Temp: {tempMin}</p>
-          <p>Stepst: {stepsMin}</p>
-      
-          <Box icon="local_drink" color ="#3A85FF" value={1.5  }unit = "L" />
-   
-          <Box icon="directions_walk" color ="black" value={3000} unit = "steps" />
-       
-          <Box icon="favorite" color ="red" value={120} unit = "bpm" />
-         
-          <Box icon="wb_sunny" color ="yellow" value={-10} unit = "°C" />
+          {/* WATER */}
+          <Box icon="local_drink" color="#3A85FF" value={this.state.water} unit="L" />
+          {/* STEPS */}
+          <Box onChange={this.onStepsChange} icon="directions_walk" color="black" value={this.state.steps} valueMin={stepsMin} valueMax={stepsmax} unit="steps" />
+          {/* HEART */}
+          <Box onChange={this.onHeartChange} icon="favorite" color="red" value={this.state.heart} valueMin={heartMin} valueMax={heartmax} unit="bpm" />
+          {/* TEMPERATURE */}
+          <Box onChange={this.onTemperatureChange} icon="wb_sunny" color="yellow" value={this.state.temperature} valueMin={tempMin} valueMax={tempMax} unit="°C" />
         </div>
       </div>
-    );
+
+    )
+
   }
 }
 
--- chapitre01/elsy/src/components/Box.js
+++ chapitre01/elsy/src/components/Box.js
@@ -1,21 +1,24 @@
 import React from "react";
 
-// const style = {
-//     fontSize: 100,
-//     color: "black"
-// };
-
-
 
 class Box extends React.Component {
 
+
+
     render() {
-        return <div className="box col-sm-3 col-6" >
-            <span style={{color: this.props.color, fontSize: 100}} className="material-icons" >
-                {this.props.icon}
-            </span>
-            <p> {this.props.value} {this.props.unit} </p>
-        </div>
+
+        return (
+            <div className="box col-sm-3 col-6" >
+                <span style={{ color: this.props.color, fontSize: 100 }} className="material-icons" >
+                    {this.props.icon}
+                </span>
+                <p> {this.props.value} {this.props.unit} </p>
+                {this.props.icon !== "local_drink" ?
+                    (<input type="range" min={this.props.valueMin} max={this.props.valueMax} value={this.props.value} onChange={this.props.onChange} />) :
+                    (null)
+                }
+            </div>
+        )
     }
 }
 
