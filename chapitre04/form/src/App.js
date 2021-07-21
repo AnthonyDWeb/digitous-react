af7f668f43c95cd3428c63e931d95ed597db86cc
AnthonyDWeb <delforge.anthony@gmail.com>
2021-07-21 16:13:51 +0200

    form react bootstrap finish

--- chapitre04/form/src/App.js
+++ chapitre04/form/src/App.js
@@ -2,31 +2,128 @@ import React from 'react';
 import './App.css';
 import  '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+
+
 class App extends React.Component{
-  render(){
-    return (
-      <div >
-          <h1 className="title">Login</h1>
-          <form className="container mx-auto" style={{width : 800}}>
-              <div className="form-group">
-                  <label for="exampleInputEmail1">Email address</label>
-                  <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                  <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+
+    constructor(){
+      super();
+
+      this.state = {
+        isLogin: false,
+        email:"",
+        emailClassName: "form-control",
+        password: "",
+        passwordClassName: "form-control"
+      }
+    }
+
+    validationEmail = (e) => {
+      let regexEmail = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/ ;
+      if (regexEmail.test(e.target.value)){
+        console.log('in')
+        this.setState((prevState)=> {
+          return {
+            ...prevState,
+            email: e.target.value,
+            emailClassName : "form-control is-valid"
+          }
+        });
+      } else {
+        console.log('out')
+        this.setState((prevState)=> {
+          return {
+            ...prevState,
+            email: e.target.value,
+            emailClassName : "form-control is-invalid"
+          }
+        });
+    }
+    }
+
+    validationPassWord = (e) => {
+      if(e.target.value.length > 5){
+        console.log('in')
+        this.setState((prevState)=> {
+          return {
+            ...prevState,
+            password : e.target.value,
+            passwordClassName : "form-control is-valid"
+          }
+        });
+      } else {
+        console.log('out')
+        this.setState((prevState)=> {
+          return {
+            ...prevState,
+            password : e.target.value,
+            passwordClassName : "form-control is-invalid"
+          }
+        });
+    }
+    }
+    
+    handleSubmit = (e) => {
+      console.log("yeah i'm")
+      e.preventDefault()
+      if (this.state.emailClassName === "form-control is-valid" && this.state.passwordClassName === "form-control is-valid"){
+        console.log("REEEEEEady")
+        this.setState((prevState)=> {
+          return {
+            ...prevState,
+            isLogin: true
+          }
+        })
+      }
+    }
+
+    renderForm() {
+      return (
+        <div className="bg">
+              <h1 className="title">Login</h1>
+              <form className="container mx-auto" style={{width : 1000}}>
+                    <div className="form-group"  style={{maxwidth : 800}}>
+                          <label htmlFor="exampleInputEmail1">Email address</label>
+                          <input onChange={this.validationEmail} type="email" className={this.state.emailClassName} id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" value={this.state.email}  required/>
+                          <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div className="form-group"  style={{maxwidth : 800}}>
+                          <label htmlFor="exampleInputPassword1">Password</label>
+                          <input onChange={this.validationPassWord} type="password" className={this.state.passwordClassName} id="exampleInputPassword1" placeholder="Password" value={this.state.password} required/>
+                    </div>
+                    <div className="form-check"  style={{maxwidth : 800}}>
+                          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                          <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                    </div>
+                    <button onClick={this.handleSubmit} type="submit" className="btn btn-primary">Submit</button>
+              </form>
+        </div>
+      )
+    }
+
+    renderFormSubmitted() {
+      return (
+        <div className="bg">
+              <h1 className="title">Login</h1>
+              <div className="form-container">
+                <h2 className="form-title">Form Submitted</h2>
               </div>
-              <div className="form-group">
-                  <label for="exampleInputPassword1">Password</label>
-                  <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-              </div>
-              <div className="form-check">
-                  <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                  <label className="form-check-label" for="exampleCheck1">Check me out</label>
-              </div>
-              <button type="submit" className="btn btn-primary">Submit</button>
-          </form>
-      </div>
-  
-    )
-  }
+        </div>
+      )
+    }
+
+    render(){
+
+        return (
+            <div>
+                {this.state.isLogin === true ? 
+                    ( this.renderFormSubmitted() ) :
+                    ( this.renderForm() )
+                }
+            </div>
+        )
+        
+    }
 }
 
 export default App;
