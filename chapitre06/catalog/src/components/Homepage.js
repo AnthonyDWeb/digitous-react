f376f6ac4065331a7f964efe1a8ba4d994d47a7c
AnthonyDWeb <delforge.anthony@gmail.com>
2021-08-04 10:11:25 +0200

    add link

--- chapitre06/catalog/src/components/Homepage.js
+++ chapitre06/catalog/src/components/Homepage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import '../App.css';
 import movies from '../movies.json'
 
@@ -6,10 +7,9 @@ import movies from '../movies.json'
 class Homepage extends React.Component{
   render(){
     return <div>
-        {/* {movies.map(movie => <p> Title: {movie.title}</p>)} */}
         {movies.map(movie => <div>
                     <img src={movie.image} alt="image" />
-                    <p>Title: {movie.title} </p>
+                    <p>Title: <Link to={`/${movie.id}`}>{movie.title}</Link></p>
                 </div>)}
     </div>
   }
