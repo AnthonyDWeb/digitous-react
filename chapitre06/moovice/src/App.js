c5ce4f3b0e3b995cfa9b025ce16f08eea139d83a
AnthonyDWeb <delforge.anthony@gmail.com>
2021-08-04 10:39:03 +0200

    initial commit

---
+++ chapitre06/moovice/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+// Style CSS
+import './App.css';
+
+import Homepage from './views/Homepage';
+import Favorite from './views/Favorite';
+import Weekly from './views/Weekly';
+import WeeklyBattle from './views/Weekly-battle';
+import Popular from './views/Popular';
+import PopularBattle from './views/Popular-battle';
+
+
+
+class App extends React.Component{
+
+  
+
+  render(){
+    return <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={Homepage} />
+            <Route exact path="/favorite" component={Favorite} />
+            <Route  path="/weekly" component={Weekly} />
+            <Route  path="/weekly-battle" component={WeeklyBattle} />
+            <Route  path="/popular" component={Popular} />
+            <Route  path="/popular-battle" component={PopularBattle} />
+        </Switch>
+    </BrowserRouter>
+  }
+}
+
+export default App;
